8fcd095c47890e90d7455b03082304099f079f69
Thej Kodiyat <tkodiyat01@gmail.com>
2019-01-17 17:03:17 -0800

    added some stateless classes/components

--- my-app-ui/src/App.jsx
+++ my-app-ui/src/App.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+class App extends React.Component {
     render() {
         return (
             <div>
-                Learn React!
+                <Header/>
+                <h2>{1+1}</h2>
+                {/*Multi line comment...*/}
+                <Content/>
+            </div>
+        )
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        var myStyle = {
+            frontSize: 100,
+            color: '#FF0000'
+        }
+        var i = 1;
+        return (
+            <div>
+                <h1 style = {myStyle}>{i == 1 ? 'True!' : 'False'}</h1>
+            </div>
+        )
+    }
+}
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <p data-myattribute = "somevalue">This is the content!!!</p>
             </div>
         )
     }
