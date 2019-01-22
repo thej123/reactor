d06a4133aae77608ccc82ae3179176bb47d41f7d
Thej Kodiyat <tkodiyat01@gmail.com>
2019-01-22 15:22:48 -0800

    added state class and fixed author

--- my-app-ui/src/App.jsx
+++ my-app-ui/src/App.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [
+                {
+                    "id":1,
+                    "name":"Foo",
+                    "age":"20"
+                },
+                {
+                    "id":2,
+                    "name":"Bar",
+                    "age":"30"
+                },
+                {
+                    "id":3,
+                    "name":"Baz",
+                    "age":"40"
+                }
+            ]
+        }
+    }
     render() {
         return (
             <div>
                 <Header/>
+                <table>
+                    <tbody>
+                        {this.state.data.map((person, i) => <TableRow key = {i} data = {person}/>)}
+                    </tbody>
+                </table>
                 <h2>{1+1}</h2>
                 {/*Multi line comment...*/}
                 <Content/>
@@ -35,6 +62,18 @@ class Content extends React.Component {
                 <p data-myattribute = "somevalue">This is the content!!!</p>
             </div>
         )
+    }
+}
+
+class TableRow extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.data.id}</td>
+                <td>{this.props.data.name}</td>
+                <td>{this.props.data.age}</td>
+            </tr>
+        );
     }
 }
 
