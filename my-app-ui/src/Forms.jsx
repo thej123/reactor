6745f3a2c3b1e2096dbfdef16b1afe8baf44a7ac
Thej Kodiyat <tkodiyat01@gmail.com>
2019-01-31 15:09:17 -0800

    Forms through Child Components
    - sent data and functions from parent to child components using props

--- my-app-ui/src/Forms.jsx
+++ my-app-ui/src/Forms.jsx
@@ -12,15 +12,27 @@ class Form extends React.Component {
     updateState(e) {
         this.setState({data: e.target.value});
     }
-
+    //Whenever we need to update state from child component, we need to pass the function that will 
+    //handle updating (updateState) as a prop (updateStateProp).
     render() {
         return (
             <div>
-                <input type="text" value = {this.state.data}
-                onChange = {this.updateState} />
-                <h4>{this.state.data}</h4>
+                <Content myDataProp = {this.state.data}
+                    updateStateProp = {this.updateState}></Content>
             </div>
         );
+    }
+}
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <input type="text" value = {this.props.myDataProp}
+                    onChange = {this.props.updateStateProp} />
+                <h3>{this.props.myDataProp}</h3>
+            </div>
+        )
     }
 }
 
